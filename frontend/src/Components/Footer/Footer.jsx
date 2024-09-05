c1fe73c0c5119c3a292524e90ac30a1729fb392f
AgasthyaShravya <shravya2484@gmail.com>
2024-09-05 17:38:15 +0530

    Custom API for the ShopCategory is used so that it will sorted based on the category

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
             </div>
         </div>
         <div className='footer-copyright'><hr/>
-            <p>Copyright@2024-All Rights Reserved.</p>
+            <p>Copyright @ 2024 - All Rights Reserved.</p>
         </div>
 
     </div>
--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -7,7 +7,7 @@ const NewsLetter = () => {
         <h1>Don’t Miss a Beat in Fashion!</h1>
         <p>Sign up for our newsletter and receive the latest trends directly via email.</p>
         <div>
-            <input type="email" placeholder='Your Email'/>
+            <input type="email" placeholder='    Your Email'/>
             <button>Join Our Family</button>
         </div>
     </div>
